b4d33ed69a93dccde5726093e87b831874836bea
Aleksi Lahtinen <aleksi.lahtinen@protonmail.com>
2024-03-10 00:21:21 +0200

    Added responsivity to chart

--- src/Components/ProfitChart.tsx
+++ src/Components/ProfitChart.tsx
@@ -1,38 +1,38 @@
 import {
-  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
+  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
 } from 'recharts'
 
 const ProfitChart = (props: any) => {
   const { data, areas } = props
   return (
-    <>
-      <AreaChart
-        width={1180}
-        height={400}
-        data={data}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        {areas.map((area: any) => (
-          <Area
-            key={area.dataKey}
-            type="monotone"
-            dataKey={area.dataKey}
-            stackId={area.stackId}
-            stroke={area.stroke}
-            fill={area.fill}
-          />
-        ))}
-      </AreaChart>
-    </>
+    <div style={{ width: '100%', height: '100%' }}>
+      <ResponsiveContainer>
+        <AreaChart
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          {areas.map((area: any) => (
+            <Area
+              key={area.dataKey}
+              type="monotone"
+              dataKey={area.dataKey}
+              stackId={area.stackId}
+              stroke={area.stroke}
+              fill={area.fill}
+            />
+          ))}
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
